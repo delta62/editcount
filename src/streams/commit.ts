d3cd1fef019ea396b974a2666a2ffdf6dfe19ae1
Sam Noedel <sam.noedel@gmail.com>
2019-02-19 22:47:06 -0800

    Emit one item for each commit

--- src/streams/commit.ts
+++ src/streams/commit.ts
@@ -1,9 +1,17 @@
-import { Observable } from 'rxjs'
+import { Observable, from } from 'rxjs'
+import { flatMap } from 'rxjs/operators'
+
+import { lines } from '../strings'
 import execStream from './exec'
 import Args from '../args'
 
 export default function revListStream(progArgs: Args): Observable<string> {
   let args = [ 'rev-list', `^${progArgs.from}`, `${progArgs.to}` ]
   let opts = { cwd: progArgs.cwd, timeout: 2 }
-  return execStream('git', args, opts) as Observable<string>
+  return (execStream('git', args, opts) as Observable<string>)
+    .pipe(flatMap(mapLines))
+}
+
+function mapLines(str: string) {
+  return from(lines(str))
 }
