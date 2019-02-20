728dfe5f98796aeee6da6d8ae35cf55d130c70ef
Sam Noedel <sam.noedel@gmail.com>
2019-02-20 07:51:18 -0800

    Add diff splitting

--- lib/streams/diff.ts
+++ lib/streams/diff.ts
@@ -1,9 +1,19 @@
-import { Observable } from 'rxjs'
+import { Observable, from } from 'rxjs'
+import { flatMap } from 'rxjs/operators'
+
 import execStream from './exec'
 import Args from '../args'
+
+const DIFF_MATCHER = /^diff --git.*\n]$/
 
 export default function diffStream(progArgs: Args, hash: string): Observable<string> {
   let args = [ 'show', '--format=%H%+ae%+an%+at', hash ]
   let opts = { cwd: progArgs.cwd, timeout: 5 }
-  return execStream('git', args, opts) as Observable<string>
+  return (execStream('git', args, opts) as Observable<string>)
+    .pipe(flatMap(mapDiffFiles))
+}
+
+function mapDiffFiles(diff: string) {
+  let matches = diff.split(DIFF_MATCHER)
+  return from(matches || [ ])
 }
